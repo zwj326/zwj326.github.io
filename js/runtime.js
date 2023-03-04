d6a654667833d88e3b6bddbd8a11d27c3a38c1fa
Space空中 <zwj326@gmail.com>
2023-03-05 05:09:44 +0800

    Site updated: 2023-03-05 05:09:44

--- js/runtime.js
+++ js/runtime.js
@@ -28,11 +28,11 @@ setInterval(() => {
   }//秒
   //早上7点到晚上10点营业
   if ((Number(time[2])<22) && (Number(time[2])>7)){
-    currentTimeHtml ="<img class='boardsign' src='https://cdn.zwj326.space/MyBlog/营业中.svg' title='距离百年老店也就差不到一百年~'><div id='runtime'>" + time[0] + ' YEAR ' + time[1] + ' DAYS ' + time[2] + ' : ' + time[3] + ' : ' + time[4] + '</div>';
+    currentTimeHtml ="<img class='boardsign' src='https://minio.zwj326.space/MyBlog/营业中.svg' title='距离百年老店也就差不到一百年~'><div id='runtime'>" + time[0] + ' YEAR ' + time[1] + ' DAYS ' + time[2] + ' : ' + time[3] + ' : ' + time[4] + '</div>';
   } //徽标内容参考站内教程
   //其余时间打烊
   else{
-    currentTimeHtml ="<img class='boardsign' src='https://cdn.zwj326.space/MyBlog/打烊了.svg' title='这个点了应该去睡觉啦，熬夜对身体不好哦'><div id='runtime'>" + time[0] + ' YEAR ' + time[1] + ' DAYS ' + time[2] + ' : ' + time[3] + ' : ' + time[4] + '</div>'; //徽标内容参考站内教程
+    currentTimeHtml ="<img class='boardsign' src='https://minio.zwj326.space/MyBlog/打烊了.svg' title='这个点了应该去睡觉啦，熬夜对身体不好哦'><div id='runtime'>" + time[0] + ' YEAR ' + time[1] + ' DAYS ' + time[2] + ' : ' + time[3] + ' : ' + time[4] + '</div>'; //徽标内容参考站内教程
   }
   //覆写挂载标签的内容
   document.getElementById("workboard").innerHTML = currentTimeHtml;
